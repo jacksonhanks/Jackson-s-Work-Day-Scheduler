31c2ca4a41c6190328497260df828ded97231029
jacksonhanks <jacksonjohnrayhanks@gmail.com>
2023-03-26 20:11:18 -0400

    edited HTML to add working hours

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(function () {
     // TODO: Add code to get any user input that was saved in localStorage and set
     // the values of the corresponding textarea elements. HINT: How can the id
     // attribute of each time-block be used to do this?
-    //
+    
     //Display the current date at page header   
     var today = dayjs();
     $('#currentDay').text(today.format('MMM D, YYYY'));
